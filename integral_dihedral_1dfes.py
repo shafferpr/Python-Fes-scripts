3c114a62a9898d3663e00a48e4aabc7e2f627e57
Patrick Shaffer <shafferpr@gmail.com>
2015-02-06 12:18:18 +0100

    added integral_dihedral_anypair.py which computes the fes for any pair of dihedrals from the bias

--- integral_dihedral_1dfes.py
+++ integral_dihedral_1dfes.py
@@ -8,14 +8,14 @@ import numpy as np
 from dihedral_fes_tools import ReadFes2D
 
 Nbiases=int(sys.argv[1])
-targetCV1=int(sys.argv[2])
-targetCV2=int(sys.argv[3])
-biasfactor=float(sys.argv[4])
-temperature=float(sys.argv[5])
+targetbias=int(sys.argv[2])
+biasfactor=float(sys.argv[3])
+temperature=float(sys.argv[4])
 #init=int(sys.argv[4])
 
 scalefactor=biasfactor/(biasfactor-1)
 kbt=2.49*temperature/300
+
 
 xg=np.zeros( [Nbiases,200] )
 yg=np.zeros( [Nbiases,200] )
@@ -31,55 +31,32 @@ spacing=3.14159/100
 
 hofx=np.zeros(200)
 
-for k in range(0, Nbiases-targetCV2, 1):
+for k in range(0, Nbiases-targetbias-1, 1):
     hofx=np.zeros(200)
     for i in range(0, xg[0].size, 1):
         for j in range(0, yg[0].size, 1):
             hofx[i]+=math.exp(scalefactor*bias[Nbiases-(1+k)][i][j]/kbt)*gofx[j]*spacing
     gofx=hofx
 
-for k in range(0, targetCV1, 1):
+for k in range(0, targetbias, 1):
     hofx=np.zeros(200)
     for i in range(0, xg[0].size, 1):
         for j in range(0, yg[0].size, 1):
             hofx[i]+=math.exp(scalefactor*bias[k][j][i]/kbt)*bofx[j]*spacing
     bofx=hofx
 
-
-
-Gofx=np.zeros( [200,200] )
-
-for i in range(0, xg[0].size, 1):
-    for j in range(0, yg[0].size, 1):
-        for q in range(0, yg[0].size, 1):
-            Gofx[i][j] += bofx[i]*math.exp(scalefactor*(bias[targetCV1][i][q]+bias[targetCV1+1][q][j])/kbt)
-
-for k in range(targetCV1+2, targetCV2, 1):
-    Hofx=np.zeros( [200,200] )
-    for i in range(0, xg[0].size, 1):
-        for j in range(0, yg[0].size, 1):
-            for q in range(0, yg[0].size, 1):
-                Hofx[i][j] +=  Gofx[i][q]*math.exp(scalefactor*bias[k][q][j]/kbt)
-    Gofx=Hofx
-
-
 fes=np.zeros( [200,200] )
 probability=np.zeros( [200,200] )
-
 integral=0
 for i in range(0, gofx.size, 1):
     for j in range(0, bofx.size, 1):
-        fes[i][j] = -kbt*math.log(Gofx[i][j])-kbt*math.log(gofx[j])
-        probability[i][j]=Gofx[i][j]+gofx[j]
-        if targetCV2 == targetCV1 +1:
-            fes[i][j]=-kbt*math.log(gofx[j])-kbt*math.log(bofx[i])-scalefactor*bias[targetCV1][i][j]
-            probability[i][j]= gofx[j]+bofx[i]+math.exp(scalefactor*bias[targetCV1][i][j]/kbt)
-        integral += probability[i][j]
+        fes[i][j] = -scalefactor*bias[targetbias][i][j]-kbt*math.log(gofx[j])-kbt*math.log(bofx[i])
+        probability[i][j]= math.exp(-fes[i][j]/kbt)
+        integral+=probability[i][j]
 
 for i in range(0, gofx.size, 1):
     for j in range(0, bofx.size, 1):
         probability[i][j]=probability[i][j]/integral
-
 
 fes1dx=np.zeros(200)
 fes1dy=np.zeros(200)
@@ -95,24 +72,21 @@ for i in range(0, xg[0].size, 1):
     fes1dy[i]=-kbt*math.log(fes1dy[i])
 
 fes_uncorrelated=np.zeros( [200,200] )
+
 probability_uncorrelated=np.zeros( [200,200] )
-
-for i in range(0, xg[0].size, 1):
-    for j in range(0, yg[0].size, 1):
-        fes_uncorrelated[i][j]=fes1dx[i]+fes1dy[j]
-
 integral=0
 for i in range(0, xg[0].size, 1):
     for j in range(0, yg[0].size, 1):
-        probability_uncorrelated[i][j]=math.exp(-fes_uncorrelated[i][j]/kbt)
-        integral+=math.exp(-fes_uncorrelated[i][j]/kbt)
-        
+        fes_uncorrelated[i][j]=fes1dx[i]+fes1dy[j]
+        probability_uncorrelated[i][j]= math.exp(-fes_uncorrelated[i][j]/kbt)
+        integral+=probability_uncorrelated[i][j]
+
 for i in range(0, xg[0].size, 1):
     for j in range(0, yg[0].size, 1):
         probability_uncorrelated[i][j]=probability_uncorrelated[i][j]/integral
 
 
-filename="fes"+str(targetCV1)+"."+str(targetCV2)
+filename="fes"+str(targetbias)
 outputfile=open(filename, "a")
 fes_min=np.amin(fes)
 fes_min2=np.amin(fes_uncorrelated)
@@ -124,18 +98,18 @@ for i  in range(0, xg[0].size,1):
 
 outputfile.close()
 
-filename="probability"+str(targetCV1)+"."+str(targetCV2)
+targetCV2=targetbias+1
+filename="probability"+str(targetbias)+"."+str(targetCV2)
 outputfile=open(filename, "a")
-
 mutualinformation=0
 for i  in range(0, xg[0].size,1):
     for j in range(0, yg[0].size,1):
         mutualinformation+=probability[i][j]*math.log(probability[i][j]/probability_uncorrelated[i][j])
-        outputfile.write('%f %f %f %f\n' %(xg[0][i], yg[0][j], probability[i][j], probability[i][j]*math.log(probability[i][j]/probability_uncorrelated[i][j])))
+        outputfile.write('%f %f %.12f %.12f\n' %(xg[0][i], yg[0][j], probability[i][j], probability[i][j]*math.log(probability[i][j]/probability_uncorrelated[i][j])))
     outputfile.write('\n')
 
 outputfile.close()
-print targetCV1, targetCV2, mutualinformation
+print mutualinformation
 
 '''
 def f(x):
